22f270462f4f4947168909ba77dbbb25bcdeadaa
Tiet Le Bao Khanh <khanh.tiet.6001@gmail.com>
2020-08-10 00:15:57 +0700

    basic demo

--- custom-attribute-directive-demo/src/app/custom-directive-demo.directive.ts
+++ custom-attribute-directive-demo/src/app/custom-directive-demo.directive.ts
@@ -1,11 +1,31 @@
-import { Directive, ElementRef } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  Input,
+} from "@angular/core";
 
 @Directive({
   selector: "[appCustomDirective]",
 })
 export class CustomDirectiveDemoDirective {
-  constructor(private el: ElementRef) {
-    this.el.nativeElement.innerText = "DEMO TEXT";
-    this.el.nativeElement.style.color = "blue";
+  @Input() hoverColor: string;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.customContent("DEMO TEXT", "blue");
+  }
+
+  @HostListener("mouseenter") onMouseEnter() {
+    this.customContent("ON MOUSE ENTER", this.hoverColor);
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.customContent("DEMO TEXT", "blue");
+  }
+
+  private customContent(text: string, color: string) {
+    this.renderer.setProperty(this.el.nativeElement, "innerText", text);
+    this.renderer.setStyle(this.el.nativeElement, "color", color);
   }
 }
